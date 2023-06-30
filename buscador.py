8cdb8e8f7a71453a18be103f079eaa845b36fd8a
Kiko <kikopro92@gmail.com>
2023-06-30 14:53:53 -0300

    Creación de variables y sus verificaciones para actualizar valores. Crear diccionario con resultados y guardar JSON

--- buscador.py
+++ buscador.py
@@ -10,7 +10,7 @@ payload = {
     "IDW": "",
     "LastNumSol": 0,
     "param1": "",
-    "param2": "1236222",
+    "param2": "1236223",
     "param3": "",
     "param4": "",
     "param5": "",
@@ -32,7 +32,11 @@ payload = {
 with requests.session() as s:
     data = json.loads(s.post(url1, json=payload).json()['d'])
 
-    data_list = [] ## Lista vacía para almacenar los datos
+    ## Se inicializan las variables
+    observada_de_fondo = False
+    fecha_observada_fondo = None
+    apelaciones = False
+    ipt = False
 
     for m in data['Marcas']:
         payload = {
@@ -42,7 +46,26 @@ with requests.session() as s:
         }
         data = json.loads(s.post(url2, json=payload).json()['d'])
         df = pd.DataFrame(data['Marca']['Instancias'])
-        data_list.append(df.to_dict()) ## Agregar los datos a la lista
 
-    with open('datos.json', 'w') as file:
-        json.dump(data_list, file) ## Guardar la lista en el archivo 'datos.json'
+        ## Verificaciones necesarias para actualizar los valores de las variables
+        if any('Resolución de observaciones de fondo de marca' in desc for desc in df['EstadoDescripcion']):
+            observada_de_fondo = True
+            fecha_observada_fondo = df.loc[df['EstadoDescripcion'].str.contains('Resolución de observaciones de fondo de marca'), 'Fecha'].values[0]
+        if any('Recurso de apelación' in desc for desc in df['EstadoDescripcion']):
+            apelaciones = True
+        if any('IPT' in desc or 'IPTV' in desc for desc in df['EstadoDescripcion']):
+            ipt = True
+
+    # Crear un diccionario con los resultados
+    resultados = {
+        "Observada de Fondo": observada_de_fondo,
+        "Fecha Observada de Fondo": fecha_observada_fondo,
+        "Apelaciones": apelaciones,
+        "IPT": ipt
+    }
+
+    # Guardar los resultados en un archivo JSON
+    with open("resultados.json", "w") as file:
+        json.dump(resultados, file)
+
+    print("Los resultados se han guardado en 'resultados.json'.")
